c0a5478b82aaa08731a8a0f351d9fbaa9965b9b8
Ivan Semenov <i.semenov8@yandex.ru>
2021-07-05 13:21:48 +0300

    facts + kinds

--- source/js/script.js
+++ source/js/script.js
@@ -7,3 +7,21 @@ navMain.classList.remove('main-nav--opened');
 navToggle.addEventListener('click', function() {
   navMain.classList.toggle('main-nav--opened');
 });
+
+
+// slow scroll for links
+
+const anchors = document.querySelectorAll('.main-nav__list a[href*="#"]')
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+  e.preventDefault()
+
+  const blockID = anchor.getAttribute('href').substr(1)
+
+  document.getElementById(blockID).scrollIntoView({
+  behavior: 'smooth',
+  block: 'start'
+  })
+  })
+}
